5a60d90760b27438ca1809dd87c544e0a3067b5e
Matthieu Vignolle <Mayeunaise@MacBook-Pro-de-Matthieu.local>
2017-04-19 16:37:41 +0200

    merge

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
-// variables
-var express = require('express');
+var express = require ('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+io.on('connection', function(socket){
+	console.log('new connection on socket')
+	console.log(socket.id)
 
-// Connection on a room
-io.on('connection', function(socket){ 
-    
-// new connection
-    console.log('New connection in here')
-    console.log(socket.id)  
-})
+	// socket.on('message', function(data){
+	// 	console.log('New event \'test\' receive with value' )
+	// 	console.log(data)
 
-//Change the url for more security
+	// 	socket.emit( 'response', {
+	// 		success: true,
+	// 		message: data.msg,
+	// 		timestamp_received : data.timestamp,
+	// 		timestamp_sent : new Date().getTime(),
+	// 		response: 'alla wuakbar'
+	// 	})
+	// });
+
+	socket.on('message', function(message){
+		console.log('received', message)
+		
+		io.emit('newmessage', message)
+
+	})
+});
+
 app.use('/static/css', express.static(__dirname + '/public/css'));
 app.use('/static/js', express.static(__dirname + '/public/js'));
 app.use('/static/images', express.static(__dirname + '/public/images'));
 app.use('/static/fonts', express.static(__dirname + '/public/fonts'));
 
-app.get('/', function(req,res){
-    res.sendFile(__dirname + '/views/index.html');
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(1337);
+server.listen(1337)
 console.log('server listening on port 1337')
